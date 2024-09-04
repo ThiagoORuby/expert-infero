9158ed3a7e799345a53d984fbc225fbb5f6fb410
ThiagoORuby <trs@ic.ufal.br>
2024-09-04 01:53:35 -0300

    fix: Corrige problemas no solver

--- expert_infero/solvers.py
+++ expert_infero/solvers.py
@@ -1,22 +1,30 @@
-from expert_infero.sentences import Implication, Sentence, Symbol, And, Or, Not
+from expert_infero.sentences import And, Implication, Not, Or, Sentence, Symbol
 
 
 def backward_chaining(
     rules: list[Implication],
     queries: list[Sentence],
-    symtable: dict[str, bool],
+    symtable: dict[str, bool | None],
     desc: dict[str, str],
 ):
     explanation: list[str] = []
 
     def ask(question: Sentence):
-        if not isinstance(question, Symbol):
+        if isinstance(question, Symbol):
+            if symtable[question.name] is False:
+                return False
+        elif isinstance(question, Not):
+            if isinstance(question.operand, Symbol):
+                if symtable[question.operand.name] is False:
+                    return True
+                question = question.operand  # faz a pergunta em relacao ao simbolo
+            else:
+                return question.evaluate(symtable)
+        else:
             return question.evaluate(symtable)
-        if symtable[question.name] is False:
-            return False
         ans = None
         while ans not in ("S", "N"):
-            ans = input(f"{question.describe(desc)} é verdade? (S/N)")[0].upper()
+            ans = input(f"{question.describe(desc)} é verdade? (S/N) ")[0].upper()
             if ans == "S":
                 symtable[question.name] = True
                 explanation.append(f"{question.describe(desc)} é verdade!")
@@ -55,9 +63,9 @@ def backward_chaining(
         for rule in rules:
             if rule.consequent == query:
                 explanation.append(
-                    f"{rule.consequent.describe(desc)} é verdade se {rule.antecedent.describe(desc)} for verdade..."
+                    f"{rule.consequent.describe(desc)} é verdade se {rule.antecedent.describe(desc)}"
                 )
-                if solve(rule.antecedent):
+                if solve(rule.antecedent) or ask(rule.antecedent):
                     # TODO: função pra validar o consequente (pode ser uma expr composta)
                     explanation.append(f"Então {query.describe(desc)} é verdade!")
                     if isinstance(query, Symbol):
@@ -70,9 +78,9 @@ def backward_chaining(
     for query in queries:
         ret = solve(query)
         if ret:
-            return explanation
+            return explanation, ret
         else:
             explanation.clear()
-    
+
     explanation.append("Não foi possível chegar a uma conclusão :(")
-    return explanation
+    return explanation, False
